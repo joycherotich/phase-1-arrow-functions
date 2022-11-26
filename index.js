c249fdf264813f76654df0d0882dfb01f38bde8a
joy <letimjoy7@gmail.com>
2022-11-26 06:52:50 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+let m=2000
+let n=100
+const divide = function (m, n){
+    return 20;
+}
+let x=2
+const square = x => {
+    return x * x;
+}
+let a=3
+let b=4
+const add = (a, b) => {
+    return a + b;
+}
